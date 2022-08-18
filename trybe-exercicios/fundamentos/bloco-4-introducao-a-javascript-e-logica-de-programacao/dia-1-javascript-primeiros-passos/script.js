7f1db17faba3eace19d9d31cd33cc11049c78e22
Laísa Rio <laisa.rioverde@gmail.com>
2022-08-18 15:04:29 -0300

    exercicio fixacao um

---
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,10 @@
+const myName = 'Laísa'
+const birthCity = 'São Paulo'
+let birthYear = 2003
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+birthYear = 2030
+console.log(birthYear)
+birthCity = 'Barra Mansa'
+console.log(birthCity)
