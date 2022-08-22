cc00f72a69f26b0d3268323f2c2113e24e78c50b
Laísa Rio <laisa.rioverde@gmail.com>
2022-08-22 12:20:17 -0300

    exercicios

---
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicio/script.js
@@ -0,0 +1,55 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+/*
+let sum = 0
+for (let i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i]
+}
+console.log(sum)
+---------------------------------------
+let mediaAritmetica = sum / numbers.length
+console.log(mediaAritmetica)
+if (mediaAritmetica > 20) {
+  console.log('Valor maior que 20')
+} else {
+  console.log('Valor menor que 20')
+}
+-----------------------------------------------
+let max = 0
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > max) {
+    max = numbers[i]
+  }
+}
+console.log(max)
+------------------------------
+
+let numImpar = 0
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] % 2 !== 0) {
+    numImpar += 1
+  }
+}
+if (numImpar === 0) {
+  console.log('Nenhum valor impar encontrado')
+} else {
+  console.log(numImpar)
+}
+-------------------------------------------------------------
+let min = numbers[0]
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] < min) {
+    min = numbers[i]
+  }
+}
+console.log(min)
+-----------------------------------------------------
+let array = []
+for (let i = 0; i <= 25; i += 1) {
+  array.push(i)
+}
+console.log(array)
+
+  */
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i] / 2)
+}
