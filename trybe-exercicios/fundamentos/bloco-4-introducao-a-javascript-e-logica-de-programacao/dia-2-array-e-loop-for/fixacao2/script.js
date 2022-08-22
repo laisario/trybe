6cc26862f203adc86872abae9e5de12042994af7
Laísa Rio <laisa.rioverde@gmail.com>
2022-08-22 10:04:54 -0300

    exercicio de fixacao 2 e 3

---
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/fixacao2/script.js
@@ -0,0 +1,9 @@
+/* let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia']
+for (let i = 0; i < groceryList.length; i += 1) {
+  console.log(groceryList[i])
+}
+*/
+let names = ['João', 'Maria', 'Antônio', 'Margarida']
+for (let nome of names) {
+  console.log(nome)
+}
