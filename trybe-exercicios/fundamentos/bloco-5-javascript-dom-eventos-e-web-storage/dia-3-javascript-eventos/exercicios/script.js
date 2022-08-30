cf09a76cdd96102afba4840286a2f0f288bdb93b
Laísa Rio <laisa.rioverde@gmail.com>
2022-08-30 12:33:57 -0300

    requisito 3

--- trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
@@ -21,32 +21,53 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek()
 
-function diasDoCalendario() {
-  let decemberDaysList = [
-    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-  ]
-  const daysList = document.querySelector('#days')
-  for (let index = 0; index < decemberDaysList.length; index += 1) {
-    const day = decemberDaysList[index]
-    const numberDayList = document.createElement('li')
-    numberDayList.innerHTML = day
-    numberDayList.className = 'day'
+let decemberDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+]
+function createDaysOfTheMonth() {
+  let daysList = document.getElementById('days')
+  for (let i = 0; i < decemberDaysList.length; i += 1) {
+    let dayListItem = document.createElement('li')
+    dayListItem.innerHTML = decemberDaysList[i]
+
+    let day = decemberDaysList[i]
+
     if (day === 24 || day === 31) {
-      numberDayList.className = 'day holiday'
-      daysList.appendChild(numberDayList)
+      dayListItem.className = 'day holiday'
     } else if (day === 4 || day === 11 || day === 18) {
-      numberDayList.className = 'day friday'
-      daysList.appendChild(numberDayList)
+      dayListItem.className = 'day friday'
     } else if (day === 25) {
-      numberDayList.className = 'day holiday friday'
-      daysList.appendChild(numberDayList)
+      dayListItem.className = 'day holiday friday'
     } else {
-      numberDayList.className = 'day'
-      daysList.appendChild(numberDayList)
+      dayListItem.className = 'day'
+    }
+
+    daysList.appendChild(dayListItem)
+  }
+}
+createDaysOfTheMonth()
+
+function createButtonHoliday(nomeButton) {
+  let buttonContainer = document.getElementsByClassName('buttons-container')[0]
+  let buttonHoliday = document.createElement('button')
+  buttonHoliday.setAttribute('id', 'bth-holiday')
+  buttonHoliday.innerHTML = nomeButton
+  buttonContainer.appendChild(buttonHoliday)
+}
+createButtonHoliday('Feriado')
+
+let buttonHoliday = document.getElementById('bth-holiday')
+
+function changeBackground() {
+  let elementosHoliday = document.getElementsByClassName('holiday')
+  for (let i = 0; i < elementosHoliday.length; i += 1) {
+    if (elementosHoliday[i].style.backgroundColor !== 'white') {
+      elementosHoliday[i].style.backgroundColor = 'white'
+    } else {
+      elementosHoliday[i].style.backgroundColor = 'rgb(238,238,238)'
     }
   }
 }
-diasDoCalendario()
 
-function feriados() {}
+buttonHoliday.addEventListener('click', changeBackground)
