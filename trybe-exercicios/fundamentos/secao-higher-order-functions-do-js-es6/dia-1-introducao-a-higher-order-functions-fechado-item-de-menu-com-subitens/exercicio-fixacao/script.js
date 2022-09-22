8703c39699a061ff8b656a40dc00b9397f94373d
Laísa Rio <laisa.rioverde@gmail.com>
2022-09-22 15:43:09 -0300

    Exercício de fixação sobre Higher Order Functions

---
+++ trybe-exercicios/fundamentos/secao-higher-order-functions-do-js-es6/dia-1-introducao-a-higher-order-functions-fechado-item-de-menu-com-subitens/exercicio-fixacao/script.js
@@ -0,0 +1,6 @@
+const dia = () => 'Acordando!!';
+const tarde = () => 'Bora tomar café!!';
+const noite = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func());
+doingThings(tarde);
