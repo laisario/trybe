a06fdd2a55ba10507c6b81348a67e4b5a33944d9
Laísa Rio <laisa.rioverde@gmail.com>
2022-12-06 21:53:36 -0300

    esqueci de comitar nas branchs certas dnv ...

--- trybe-exercicios/front-end/secao-7-gerenciamento-de-estado-com-redux/dia-2-usando-redux-no-react/my-app/src/App.js
+++ trybe-exercicios/front-end/secao-7-gerenciamento-de-estado-com-redux/dia-2-usando-redux-no-react/my-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { actionCreator } from './redux/actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const mapStateToProps = (state) => ({
+  countState: state.count,
+});
 
-export default App;
+class App extends React.Component {
+  render() {
+  const { dispatch, countState } = this.props;
+      return (
+        <div>
+          <h1>Contador</h1>
+          <h2>{ countState }</h2>
+          <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+          <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
+        </div>
+      );
+    }
+  }
+  
+  export default connect(mapStateToProps)(App);
+  
