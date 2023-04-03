cc430222ede2b88f67dbe5f2864ca06bdd55cef4
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-04-03 11:11:07 -0300

    nao consigo conectar no banco de dados

---
+++ trybe-exercicios/back-end/secao-6-nodejs-orm-autenticacao/dia-1-orm/exercise/models/index.js
@@ -0,0 +1,5 @@
+// // ...
+// /* Praticamente é só mudar a extensão de .json para .js! */
+// const config = require(__dirname + '/../config/config.js')[env]; // configuração nova
+
+// module.exports = config;
